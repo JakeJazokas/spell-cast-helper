f837b4702886d7b95a29549e35256dcb04df959d
Jake <jakejazokas@outlook.com>
2022-11-10 13:28:55 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Example
-An example greeter plugin
+# Spell Cast Helper RuneLite Plugin
+This plugin shows the remaining casts of the last spell that was cast. 
--- src/test/java/com/spellcasthelper/SpellCastHelperPluginTest.java
+++ src/test/java/com/spellcasthelper/SpellCastHelperPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.spellcasthelper;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class SpellCastHelperPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(SpellCastHelperPlugin.class);
 		RuneLite.main(args);
 	}
 }
